b5be7a4991aab5c196182644016c1d527a50e991
Tom Dudzik <tdudz97@gmail.com>
2016-07-12 02:43:56 -0400

    added setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='libml-py',
+      version='0.1',
+      packages=['libml-py'],
+      license='MIT',
+      url='https://github.com/tdudz/libml-py',
+      author='Tom Dudzik',
+      description='A machine learning library in python, written from scratch',
+      keywords='machine learning python library'
+      install_requires=['numpy'])
