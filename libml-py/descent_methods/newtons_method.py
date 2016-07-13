9736d64e048dd8496f0fb9a6aa3266d79cc3c97a
Tom Dudzik <tdudz97@gmail.com>
2016-07-13 19:12:40 -0400

    added newtons method

--- libml-py/descent_methods/newtons_method.py
+++ libml-py/descent_methods/newtons_method.py
@@ -2,7 +2,6 @@
 Newton's Method
 ---------------
 
-
 """
 
 import numpy as np
@@ -14,7 +13,7 @@ def newtons_method(X, y, gradient, hessian, learning_rate=0.01, iterations=10000
 	Args:
 		X (np.ndarray): Training data with shape [n_samples, n_features]
 		y (np.ndarray): Target values with shape [n_samples, 1]
-		gradient (func): Function used to compute the gradient, is a function of (X, y, weights)
+		gradient (func): Function used to compute the gradient, is a function of (X, y, theta)
 		hessian (func): Function used to compute the hessian, is a function of (X)
 		learning rate (float): Step size used during each iteration
 		iterations (int): Number of iterations to perform
@@ -22,3 +21,8 @@ def newtons_method(X, y, gradient, hessian, learning_rate=0.01, iterations=10000
 	Returns:
 		np.ndarray: A np array of weights with shape [n_features, 1]
 	"""
+	theta = np.zeros((len(X[0]),1))
+	for _ in xrange(iterations):
+		newton_step = np.linalg.pinv(hessian(X)).dot(gradient(X, y, theta))
+		theta = np.subtract(theta, learning_rate * newton_step)
+	return theta
