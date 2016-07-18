fixes to gradient descents, allow for custom weight input

libml-py/descent_methods/batch_gradient_descent.py
@@ -9,7 +9,7 @@ with respect to the parameters for the entire training dataset.
 
 import numpy as np
 
-def batch_gradient_descent(X, y, gradient, learning_rate=0.01, iterations=1000):
+def batch_gradient_descent(X, y, gradient, theta=None, learning_rate=0.01, iterations=1000):
 	"""
 	Performs batch gradient descent for a set number of iterations.
 
@@ -17,13 +17,15 @@ def batch_gradient_descent(X, y, gradient, learning_rate=0.01, iterations=1000):
 		X (np.ndarray): training data with shape [n_samples, n_features]
 		y (np.ndarray): target values with shape [n_samples, 1]
 		gradient (func): function used to compute the gradient, a function of (X, y, weights)
+		theta (np.ndarray): np array of weights/parameters to update
 		learning rate (float): step size used during each iteration
 		iterations (int): number of iterations to perform
 
 	Returns:
-		np.ndarray: an array of weights with shape [n_features, 1]
+		np.ndarray: a np array of weights with shape [n_features, 1]
 	"""
-	theta = np.zeros((len(X[0]),1))
+	if theta is None:
+		theta = np.zeros((len(X[0]),1))
 	for _ in xrange(iterations):
 		theta = np.subtract(theta, learning_rate * gradient(X, y, theta))
 	return theta

libml-py/descent_methods/minibatch_gradient_descent.py
@@ -8,7 +8,7 @@ parameters for a set of n training examples.
 
 import numpy as np
 
-def minibatch_gradient_descent(X, y, gradient, batch_size, learning_rate=0.01, iterations=1000):
+def minibatch_gradient_descent(X, y, gradient, batch_size, theta=None, learning_rate=0.01, iterations=1000):
     """
     Performs minibatch gradient descent for a set number of iterations.
 
@@ -16,6 +16,7 @@ def minibatch_gradient_descent(X, y, gradient, batch_size, learning_rate=0.01, i
         X (np.ndarray): training data with shape [n_samples, n_features]
         y (np.ndarray): target values with shape [n_samples, 1]
         gradient (func): function used to compute the gradient, a function of (X, y, weights)
+        theta (np.ndarray): np array of weights/parameters to update
         batch_size (int): size of the individual minibatch
         learning rate (float): step size used during each iteration
         iterations (int): number of iterations to perform
@@ -23,7 +24,8 @@ def minibatch_gradient_descent(X, y, gradient, batch_size, learning_rate=0.01, i
     Returns:
         np.ndarray: a np array of weights with shape [n_features, 1]
     """
-    theta = np.zeros((len(X[0]),1))
+    if theta is None:
+        theta = np.zeros((len(X[0]),1))
     for _ in xrange(iterations):
         shuffled = map(np.random.permutation, X)
         for i in xrange(0,len(y),batch_size):

libml-py/descent_methods/newtons_method.py
@@ -1,12 +1,13 @@
 """
 Newton's Method
 ---------------
+Calculates the optimal step size based on the Hessian matrix.
 
 """
 
 import numpy as np
 
-def newtons_method(X, y, gradient, hessian, learning_rate=0.01, iterations=10000):
+def newtons_method(X, y, gradient, hessian, theta=None, iterations=10000):
 	"""
 	Performs newton's method for a set number of iterations, with a constant step size.
 
@@ -15,14 +16,15 @@ def newtons_method(X, y, gradient, hessian, learning_rate=0.01, iterations=10000
 		y (np.ndarray): target values with shape [n_samples, 1]
 		gradient (func): function used to compute the gradient, is a function of (X, y, theta)
 		hessian (func): function used to compute the hessian, is a function of (X)
-		learning rate (float): step size used during each iteration
+		theta (np.ndarray): np array of weights/parameters to update
 		iterations (int): number of iterations to perform
 
 	Returns:
 		np.ndarray: a np array of weights with shape [n_features, 1]
 	"""
-	theta = np.zeros((len(X[0]),1))
+	if theta is None:
+		theta = np.zeros((len(X[0]),1))
 	for _ in xrange(iterations):
 		newton_step = np.linalg.pinv(hessian(X)).dot(gradient(X, y, theta))
-		theta = np.subtract(theta, learning_rate * newton_step)
+		theta = np.subtract(theta, newton_step)
 	return theta

libml-py/descent_methods/stochastic_gradient_descent.py
@@ -17,13 +17,15 @@ def stochastic_gradient_descent(X, y, gradient, learning_rate=0.01, iterations=1
 		X (np.ndarray): training data with shape [n_samples, n_features]
 		y (np.ndarray): target values with shape [n_samples, 1]
 		gradient (func): function used to compute the gradient, a function of (X, y, weights)
+		theta (np.ndarray): np array of weights/parameters to update
 		learning rate (float): step size used during each iteration
 		iterations (int): number of iterations to perform
 
 	Returns:
 		np.ndarray: a np array of weights with shape [n_features, 1]
 	"""
-	theta = np.zeros((len(X[0]),1))
+	if theta is None:
+		theta = np.zeros((len(X[0]),1))
 	for _ in xrange(iterations):
 		locs = list(range(len(y)))
 		np.random.shuffle(locs)